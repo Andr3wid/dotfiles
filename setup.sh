07d047c9e596f82b4d816655da6d6b1ac5c548cb
Andreas Kogler <andy.kogler.junior@gmail.com>
2020-12-22 20:43:21 +0100

    Add rofi configuration.

--- setup.sh
+++ setup.sh
@@ -68,6 +68,7 @@ ln -sf $SCRIPT_DIR/.Xresources /home/$USERNAME/.Xresources && xrdb /home/$USERNA
 ln -sf $SCRIPT_DIR/starship.toml /home/$USERNAME/.config/starship.toml
 ln -sf $SCRIPT_DIR/i3 /home/$USERNAME/.config/i3 && chmod +x launch_polybar.sh
 ln -sf $SCRIPT_DIR/.bashrc /home/$USERNAME/.bashrc
+ln -sf $SCRIPT_DIR/rofi /home/$USERNAME/.config/rofi
 echo "- copying system config files"
 if [ -f $TOUCHPAD_CONF ]
 	then mv $TOUCHPAD_CONF $TOCHPAD_CONF.bak
